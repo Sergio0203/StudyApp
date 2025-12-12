5eee5f683b2395b24ef449f580305cb27008c42e
Sergio Cesar <sergiocesar0203@gmail.com>
2025-12-12 15:34:11 -0400

    first commit

---
+++ StudyApp/StudyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAppApp.swift
+//  StudyApp
+//
+//  Created by Sérgio César Lira Júnior on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
